7efee3528fd59d1ce03fec3c1576839f009dae23
Qinisela Molefe <molefeq@gmail.com>
2024-07-29 21:49:06 +0200

    Updating config

--- supabase/scripts/application_start.sh
+++ supabase/scripts/application_start.sh
@@ -13,7 +13,8 @@ cp -R $CLONE_ME_BASE_DIRECTORY/functions $DOCKER_BASE_DIRECTORY/volumes
 cp $CLONE_ME_BASE_DIRECTORY/env-config/.env.dev .env
 
 echo "REFRESH DOCKER CONTAINER"
-docker-compose down
+docker-compose down -v
+rm -rf volumes/db/data/
 docker-compose up -d
 
 echo "STARTING THE DB MIGRATION"
