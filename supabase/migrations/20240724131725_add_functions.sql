b216b1159b37d325e86a1bcb21108b67e5ccf9d6
Qinisela Molefe <molefeq@gmail.com>
2024-07-30 07:12:05 +0200

    update environment config

--- supabase/migrations/20240724131725_add_functions.sql
+++ supabase/migrations/20240724131725_add_functions.sql
@@ -1,22 +1,3 @@
-
-INSERT INTO user_roles
-(id, role_type, description)
-VALUES
-    (1, 'client', 'The paying customer who creates the errands'),
-    (2, 'runner', 'Person doing the errands'),
-    (3, 'admin', 'System Administrator');
-
---bucket for errands
-INSERT INTO
-    storage.buckets (id, name, public, allowed_mime_types)
-VALUES
-    (
-        'errand_media',
-        'errand_media',
-        true,
-        '{"audio/mp4", "audio/mpeg", "audio/m4a", "audio/aac"}'
-    );
-
 drop function if exists public.accept_quote;
 
 create or replace function public.accept_quote(client_quote_id integer)
