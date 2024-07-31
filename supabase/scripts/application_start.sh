d247c8790cf37fd643fb4f641538bdf845a04132
Qinisela Molefe <molefeq@gmail.com>
2024-07-31 12:07:35 +0200

    update environment config

--- supabase/scripts/application_start.sh
+++ supabase/scripts/application_start.sh
@@ -11,9 +11,11 @@ mkdir -p $DOCKER_BASE_DIRECTORY/db/migrations
 cp -R $CLONE_ME_BASE_DIRECTORY/migrations $DOCKER_BASE_DIRECTORY/db
 cp -R $CLONE_ME_BASE_DIRECTORY/functions $DOCKER_BASE_DIRECTORY/volumes
 cp -R $CLONE_ME_BASE_DIRECTORY/docker $DOCKER_BASE_DIRECTORY
+cd $CLONE_ME_BASE_DIRECTORY/scripts
 pip install boto3
 pip install python-dotenv
-python3 $CLONE_ME_BASE_DIRECTORY/scripts/env-update.py
+python3 ./env-update.py
+cd $DOCKER_BASE_DIRECTORY
 
 # cp $CLONE_ME_BASE_DIRECTORY/env-config/.env.dev .env
 
